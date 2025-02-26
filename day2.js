72a0a9c10bd64d942ed9950f9e4a3ed31aef2bdc
Anuj Kumar Sahu <161107569+anujsahu01@users.noreply.github.com>
2025-02-27 00:49:12 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,22 @@
+// console.log('helo js');
+console.log('Hello, World!'); 
+let b = 290;
+console.log(b);
+console.log(typeof b);
+
+console.log(typeof null);
+console.log(typeof undefined)
+
+
+// "use strict"; // isse code wrong nhi likh sakte hai !
+//  let a = 10;
+// console.log("heelo js" ,a);
+// alert( "hello journeyof my language");
+
+let d = 10 ;
+let c = 20;
+let sum = d+c;
+let mul = d*c;
+ console.log(sum);
+ console.log(mul);
+ 
---
+++ day3.js
@@ -0,0 +1,80 @@
+// // let score = "33abc";
+// let score = null;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let char = "33";
+// console.log(typeof char);
+// console.log(typeof (char));
+
+
+// console.log(score);
+// let newid = Number(score); // 33abe pring hoga
+// console.log(typeof newid); // number print hoga
+
+// console.log(newid); // NaN print hoga 
+
+
+// // let logedin = 0; 
+
+// // let logedin = "anuj"; // if space y name hoga  to true aayga
+
+// let logedin = ""; //blank string me false aayga 
+// // let islog = String(logedin);
+
+
+// let islog = Boolean(logedin);
+// console.log(islog);
+
+
+
+
+// let value = 8;
+// let negativevalue = - value;
+// console.log(negativevalue);
+
+
+// let str = 'anuj';
+// let str2 = 'sahu';
+// let str3 = str+ str2;
+// console.log(str3);
+
+
+// let a = 21;
+// let d = 21;
+// let b = '21';
+// // let c = a+d+b ; agr string aad me lgi ho to pahle wala add hoga then cpncate hoga 4221
+
+// let c = b+a+d ; // agr string pahle diya ho to caoncate hoga 212121
+// console.log(c);
+
+
+// let a = 10 ;
+// // a++;
+// ++a;
+// a++;
+// ++a;
+// console.log(a);
+
+
+// console.log(null > 0);
+
+// console.log(null == 0);
+
+// console.log(null >= 0);
+
+let a = 12;
+let b = '12';
+if (a==b){
+    console.log(true); /// == check equal or not but === check with datatype
+    
+}
+else 
+    console.log(false);
+    
+
+
+
+console.log(undefined === 0);
+
